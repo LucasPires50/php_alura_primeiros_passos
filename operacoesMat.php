5be1445681b18b092df1935d373d65d661f1c87d
Lucas Pires <luc_50@outlook.com>
2020-12-18 17:49:24 -0300

    Operações Matemátematicas

---
+++ operacoesMat.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 21;
+$idadeDaqui10Anos = $idade + 10;
+
+$idadeHa5Anos = $idade - 5;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$potencia = 2 ** 3;
+$restaDaDivisao = 10 % 3;
+
+echo $restaDaDivisao;
