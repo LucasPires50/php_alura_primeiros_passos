f9f677a8037faa06492ca83fea02f111088792ba
Lucas Pires <luc_50@outlook.com>
2020-12-20 12:40:30 -0300

    tipos de dados
    PHP possui tipos de dados como integer, double, string ou boolean (tipos primitivos)

---
+++ tiposDeDados.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 21; //interger
+$salario = 1000.30; //double ou float
+$divisao = 10 / 3;
+$texto = "Olá Mundo"; //string
+$verdadeiro = true; // boolean
+$falso = false; // boolean
+
+
+// Irá exibir o tipo da varivel passada como parametro
+echo gettype($verdadeiro);
