22cc039d697dde45ee85d5a379d848e2d3668ea2
Lucas Pires <luc_50@outlook.com>
2020-12-20 18:04:10 -0300

    else if

--- decisoes2.php
+++ decisoes2.php
@@ -1,29 +1,23 @@
 <?php
 
-$idade = 20;
-$nome = "Vinicius";
+$idade = 17;
 $numeroDePessoas = 2;
 
 echo "Você só pode entrar se tiver a parti de 18 anos. " . PHP_EOL;
 echo "ou a parti de 16 anos acompanhado(a)." . PHP_EOL;
 
-/** 
- * OU
- * if ($idade == 18 || $idade > 18)
- * if ($idade == 18 or $idade > 18)
- * 
- * E
- * if ($idade >= 18 && $nome == 'Vinicius')
- * if ($idade >= 18 and $nome == 'Vinicius')
-*/
-if ($idade >= 18 ) {
+/**
+ * Se para cada verificação tiver apenas 1 instrução,
+ * não é necessário colocar as chaves, pois o php consegue 
+ * identificar e excutar o bloco condicinal
+ */
+if ($idade >= 18 ) 
     echo "Voce tem $idade anos. Pode entrar sozinho";
-// o 'else if' -> pode ser usado dessa forma também 'elseif'
-} else if ($idade >= 16 and $numeroDePessoas > 1){
+else if ($idade >= 16 and $numeroDePessoas > 1)
     echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
-}else{
+else
     echo "Você só tem $idade anos. Você não pode entrar";
-}
+
 
 echo PHP_EOL;
 ECHO "Adeus!";
