56561bb92c817587e08f30a557d460ffb0daea04
Lucas Pires <luc_50@outlook.com>
2020-12-20 18:05:21 -0300

    operador ternário

--- operadorTernario.php
+++ operadorTernario.php
@@ -1,4 +1,6 @@
 <?php
+// https://www.php.net/manual/en/language.operators.comparison.php
+
 $idade = 15;
 $mensagem = $idade < 18 ? "Você é menor de idade" : "Você é maior de idade";
 echo $mensagem;
