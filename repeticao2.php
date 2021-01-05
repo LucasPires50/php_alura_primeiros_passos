d9ec5158bede102503f31b0a9cadde2a3e9942fd
Lucas Pires <luc_50@outlook.com>
2021-01-04 21:35:33 -0300

    continue || break

--- repeticao2.php
+++ repeticao2.php
@@ -4,6 +4,12 @@
 
 //  estrutura de repetição
 for ($contador=1; $contador <= 15 ; $contador++) { 
+
+    if ($contador == 13){
+        // continue; // pula o 13 continua o loop
+        break; // para o loop no 12
+    }
+    
     echo "#$contador" . PHP_EOL;
 }
 
