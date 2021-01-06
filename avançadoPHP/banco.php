1f7df740bb945c093648e6fc9eba08a46c8af963
Lucas Pires <luc_50@outlook.com>
2021-01-06 20:55:03 -0300

    função sacar

--- avançadoPHP/banco.php
+++ avançadoPHP/banco.php
@@ -15,18 +15,21 @@ $contasConrrentes = [
     ]
 ];
 
+function sacar($conta, $valorASacar){
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem(mensagem:"Saldo insuficiente");
+    }else{
+        $conta['saldo'] -= 500;
+    }    
+    return $conta;
+}
+
 function exibeMensagem($mensagem){
     echo $mensagem . PHP_EOL;
 }
 
-// saque
-$contasConrrentes['123.456.789-63']['saldo'] -= 500;
-if (500 > $contasConrrentes['123.456.789-65']['saldo']) {
-    exibeMensagem(mensagem:"Saldo insuficiente");
-}else{
-    $contasConrrentes['123.456.789-65']['saldo'] -= 500;
-}
-
+$contasConrrentes['123.456.789-63'] = sacar($contasConrrentes['123.456.789-63'], 500);
+$contasConrrentes['123.456.789-65'] = sacar($contasConrrentes['123.456.789-65'], 500);
 
 foreach ($contasConrrentes as $cpf => $conta) {
 
