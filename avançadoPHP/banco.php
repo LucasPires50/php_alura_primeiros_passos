1a0571a264320edd3a4546bf4bc3f43ab34f8c42
Lucas Pires <luc_50@outlook.com>
2021-01-06 21:14:45 -0300

    função depositar

--- avançadoPHP/banco.php
+++ avançadoPHP/banco.php
@@ -15,21 +15,35 @@ $contasConrrentes = [
     ]
 ];
 
-function sacar($conta, $valorASacar){
+function sacar(array $conta, float $valorASacar): array{
     if ($valorASacar > $conta['saldo']) {
-        exibeMensagem(mensagem:"Saldo insuficiente");
+        exibeMensagem("Saldo insuficiente");
     }else{
         $conta['saldo'] -= 500;
     }    
     return $conta;
 }
 
-function exibeMensagem($mensagem){
+function depositar(array $conta, float $valorADepositar): array{
+
+    if($valorADepositar > 0){
+        $conta['saldo'] += $valorADepositar;
+        exibeMensagem("Deposito efetuado!!!!!!!!!!");
+    }else{
+        exibeMensagem("Depositos precisam ser positivo.");
+    }
+    
+    return $conta;
+}
+
+function exibeMensagem(string $mensagem){
     echo $mensagem . PHP_EOL;
 }
 
 $contasConrrentes['123.456.789-63'] = sacar($contasConrrentes['123.456.789-63'], 500);
 $contasConrrentes['123.456.789-65'] = sacar($contasConrrentes['123.456.789-65'], 500);
+
+$contasConrrentes['123.456.789-64'] = depositar($contasConrrentes['123.456.789-64'], 900);
 
 foreach ($contasConrrentes as $cpf => $conta) {
 
