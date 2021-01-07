5cd3106ca6b2e994bf2dec197ea10983131162a8
Lucas Pires <luc_50@outlook.com>
2021-01-06 22:15:35 -0300

    referência

--- avançadoPHP/banco/funcoes.php
+++ avançadoPHP/banco/funcoes.php
@@ -25,10 +25,11 @@ function exibeMensagem(string $mensagem){
     echo $mensagem . PHP_EOL;
 }
 // Se não usar o '&' é passado a copia do valor da conta
-// Mas se usar o '&' é passado exatamente o valor da conta como referencia, onde se torna possivel midificação se aplicada
+// Mas se usar o '&' é passado exatamente o valor da conta como referencia(endereço onde esta variável está armazenada na memória), onde se torna possivel midificação se aplicada.
 function titularComLetrasMaiusculas(array &$conta){
     // biblioteca mbString
     // para mudar a string para letras maiusculas
+    // A função strtoupper não colocaria letras com acento em maiúsculo, enquanto a mb_strtoupper consegue fazer isso sem problemas.
     $conta['titular'] = mb_strtoupper($conta['titular']);
     
 }
