b73f135bd3267509b9417538c15be68199fd3f01
Lucas Pires <luc_50@outlook.com>
2021-01-17 18:16:04 -0300

    método contrutor

--- orientacaoAObjetos/banco.php
+++ orientacaoAObjetos/banco.php
@@ -3,12 +3,15 @@
 require_once 'src/Conta.php';
 
 $primeriaConta = new Conta("123.456.789-99", "Lucas");
-var_dump($primeriaConta);
 $primeriaConta->depositar(500);
 $primeriaConta->sacar(300);
 
 echo "Titular: {$primeriaConta->recuperarNomeTitular()}" .PHP_EOL;
 echo "Cpf: {$primeriaConta->recuperarCpfTitular()}" .PHP_EOL;
-echo "Saldo da conta: {$primeriaConta->recuperSaldo()}";
+echo "Saldo da conta: {$primeriaConta->recuperSaldo()}" .PHP_EOL;
+
+$segundaConta = new Conta("789.456.123-33", "Patricia");
+
+var_dump($segundaConta);
 
 ?>
