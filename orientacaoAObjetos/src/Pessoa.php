1f4b06abf932f5806eee0797ec0badbabb4bfe49
Lucas Pires <luc_50@outlook.com>
2021-01-18 22:02:24 -0300

    Reaproveitando código, usando herança

---
+++ orientacaoAObjetos/src/Pessoa.php
@@ -0,0 +1,36 @@
+<?php
+
+class Pessoa
+{
+    public string $nome;
+    public CPF $cpf;
+
+    public function __construct(string $nome, CPF $cpf)
+    {
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+    public function recuperarNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function recuperarCpf(): CPF
+    {
+        return $this->cpf->recuperarNumero();
+    }
+
+    
+    public function validarNomeTitular(string $nomeTitular)
+    {
+        // strlen -> pega a quantidade de caracteres 
+        if(strlen($nomeTitular) < 5){
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+}
+
+
+?>
--- orientacaoAObjetos/src/Titular.php
+++ orientacaoAObjetos/src/Titular.php
@@ -1,8 +1,6 @@
 <?php
-class Titular
+class Titular extends Pessoa
 {
-    private CPF $cpf;
-    private string $nome;
     private Endereco $endereco;
 
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
@@ -11,25 +9,6 @@ class Titular
         $this->validarNomeTitular($nome);
         $this->nome = $nome;
         $this->endereco = $endereco;
-    }
-
-    public function recuperarCpf(): string
-    {
-        return $this->cpf->recuperarNumero();
-    }
-
-    public function recuperarNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validarNomeTitular(string $nomeTitular)
-    {
-        // strlen -> pega a quantidade de caracteres 
-        if(strlen($nomeTitular) < 5){
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
     }
 
     public function recuperarEndereço() : Endereco
