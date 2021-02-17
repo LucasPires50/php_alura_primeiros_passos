7ac8c9b91f3a835ea61cb89e4d6c8bb51feb8d93
Lucas Pires <luc_50@outlook.com>
2021-02-17 20:53:50 -0300

    removido atributo cargo
    Em si as classes já representam o cargo, não sedo mais necessário armazenar esse objeto em uma variável.

--- orientacaoAObjetos/bonificacoes.php
+++ orientacaoAObjetos/bonificacoes.php
@@ -6,13 +6,13 @@ use Alura\Banco\Modelo\Service\ControladorDeBonificacoes;
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Funcionario\{Gerente, Diretor, Desenvolvedor};
 
-$umFuncinario = new Desenvolvedor('Vinicius Dias', new CPF('123.456.789-96'), 'Desenvolvedor', 1000);
+$umFuncinario = new Desenvolvedor('Vinicius Dias', new CPF('123.456.789-96'), 1000);
 
 $umFuncinario->sobeDeNivel();
 
-$funcinarioDois = new Gerente('Lais Dias', new CPF('145.852.789-38'), 'Gerente', 3000);
+$funcinarioDois = new Gerente('Lais Dias', new CPF('145.852.789-38'), 3000);
 
-$funcinariotres = new Diretor('Roberto Dias', new CPF('753.896.789-25'), 'Diretor', 5000);
+$funcinariotres = new Diretor('Roberto Dias', new CPF('753.896.789-25'), 5000);
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncinario);
--- orientacaoAObjetos/src/Modelo/Funcionario/Funcionario.php
+++ orientacaoAObjetos/src/Modelo/Funcionario/Funcionario.php
@@ -8,20 +8,13 @@ use Alura\Banco\Modelo\{CPF, Pessoa};
 abstract class Funcionario extends Pessoa
 {
 
-    private string $cargo;
     private float $salario;
 
-    public function __construct(string $nome, CPF $cpf ,string $cargo, float $salario)
+    public function __construct(string $nome, CPF $cpf, float $salario)
     {
         // Chamando o construtor da classe base(Pessoa)
         parent::__construct($nome, $cpf);
-        $this->cargo = $cargo;
         $this->salario = $salario;
-    }
-
-    public function recuperaCargo(): string
-    {
-        return $this->cargo;
     }
 
     public function alterarNome(string $nome): void
