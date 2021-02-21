519b4c41c28f0570daadff8b5e7e7234f198513b
Lucas Pires <luc_50@outlook.com>
2021-02-21 19:30:55 -0300

    Array Associativo

--- manipulandoColecoesComArrays/ArrayAssociativo/index.php
+++ manipulandoColecoesComArrays/ArrayAssociativo/index.php
@@ -2,7 +2,7 @@
 
 echo "<h1>Função array_diff</h1>";
 
-$conrrentista = [
+$conrrentistas = [
     "Giovanni",
     "João",
     "Maria",
@@ -17,8 +17,8 @@ $conrrentistaNaoPagante = [
     "Rafael"
 ];
 
-// a função retira a difireça entre os arrays
-$conrrentistaPagantes = array_diff($conrrentista, $conrrentistaNaoPagante);
+// a função retira a difireça entre os arrays, e retornar um novo array
+$conrrentistaPagantes = array_diff($conrrentistas, $conrrentistaNaoPagante);
 
 echo "<pre>";
 var_dump($conrrentistaPagantes);
@@ -26,4 +26,25 @@ echo "</pre>";
 
 
 echo "<h1>Array Associativo</h1>";
+
+$saldos = [
+    2500,
+    3000,
+    4400,
+    1000,
+    8700,
+    9000
+];
+// a função juntas os arrays, e retornar um novo array
+$juntaArray = array_merge($conrrentistas, $saldos);
+
+// a função juntas os arrays, e retornar um novo array
+$relacionados = array_combine($conrrentistas, $saldos);
+
+$relacionados["Mateus"] = 4500;
+
+echo "<pre>";
+var_dump($relacionados);
+echo "O saldo da Maria é {$relacionados["Maria"]}";
+echo "</pre>";
 ?>
