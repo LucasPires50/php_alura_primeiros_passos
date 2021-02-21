1ff632f338b35c90286d8b3c7c73545d7d1d30b6
Lucas Pires <luc_50@outlook.com>
2021-02-20 21:25:37 -0300

    função sort() para ordenar o array do menor para o maior

--- manipulandoColecoesComArrays/saldo/index.php
+++ manipulandoColecoesComArrays/saldo/index.php
@@ -11,15 +11,16 @@ $saldos = [
 ];
 
 foreach ($saldos as $saldo){
-    echo "<p>O saldo é $saldo</p>";
+    echo "<p>O saldo é $saldo;</p>";
 }
 
-echo "<pre>";
-var_dump($saldos);
+
 
 // função sort() para ordenar o array do menor para o maior
 sort($saldos);
 
-var_dump($saldos);
-echo "</pre>";
+echo "<p>O menor saldo é $saldos[0].</p>";
+
+
+
 ?>
