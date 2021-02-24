584dea5b0678bbced0fe1ef8c1cc8a435523d17c
Lucas Pires <luc_50@outlook.com>
2021-02-23 21:42:15 -0300

    Try - catch

--- PHPExceptionsTratamentodeErros/pilha.php
+++ PHPExceptionsTratamentodeErros/pilha.php
@@ -3,12 +3,18 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    // Função para deixar um array com o tamanho fixo
-    // $arrayFixo = new SplFixedArray(2);
-    // $arrayFixo[3] ='Valor'; 
+
+    
+    try {
+        // Função para deixar um array com o tamanho fixo
+        $arrayFixo = new SplFixedArray(2);
+        $arrayFixo[3] = 'Valor';
+    } catch (RuntimeException $problema) {
+        echo "Aconteceu um erro na função 1" . PHP_EOL;
+    }
 
     // Esse função sempre trata os numeros com inteiros
-    $divisao = intdiv(5, 0 );
+    // $divisao = intdiv(5, 0 );
     funcao2();
     echo 'Saindo da função 1' . PHP_EOL;
 }
@@ -16,18 +22,16 @@ function funcao1()
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
-    
-    for ($i=0; $i <= 5; $i++) { 
+
+    for ($i = 0; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
     // Formas mais simples de fazer o debuggind
-    var_dump(debug_backtrace());
-    print_r(debug_backtrace());
+    // var_dump(debug_backtrace());
+    // print_r(debug_backtrace());
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
 echo 'Iniciando o programa principal' . PHP_EOL;
 funcao1();
-echo 'Finalizando o programa principal'. PHP_EOL;
-
-?>
+echo 'Finalizando o programa principal' . PHP_EOL;
