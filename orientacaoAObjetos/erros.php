299c082fedb0f4595a4acf330633425915372497
Lucas Pires <luc_50@outlook.com>
2021-03-07 18:50:40 -0300

    Configurações de erros
    https://www.youtube.com/watch?v=YO3PRChajWc
    
    https://www.php.net/manual/pt_BR/language.errors.basics.php

---
+++ orientacaoAObjetos/erros.php
@@ -0,0 +1,15 @@
+<?php
+
+// para definir que todos os tipos de erro sejam mostrados
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', 'var/log/minha-aplicacao');
+
+
+echo $varialvel;
+echo $array[12];
+
+echo CONSTANT;
+
+?>
