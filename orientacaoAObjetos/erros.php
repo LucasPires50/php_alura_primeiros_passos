d9f9237d8014d2b67ba7d4ff3ceab302c74030b2
Lucas Pires <luc_50@outlook.com>
2021-03-07 18:53:38 -0300

    supressore de erro

--- orientacaoAObjetos/erros.php
+++ orientacaoAObjetos/erros.php
@@ -3,12 +3,10 @@
 // para definir que todos os tipos de erro sejam mostrados
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', 'var/log/minha-aplicacao');
-
 
 echo $varialvel;
-echo $array[12];
+// colocar o arroba antes serve como um supressor de erros, nunca usar isso.
+echo @$array[12];
 
 echo CONSTANT;
 
